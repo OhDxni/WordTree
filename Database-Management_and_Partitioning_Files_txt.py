7ff5e069ade184546d5310228a4740dfc010853b
Mariela Pluutus <mariela.pluutus@gmail.com>
2024-11-13 16:51:44 +0100

    Adding functions of the game to graph traversal, that can be called during game,choose wprds- generates start and end word, all_possible_next_word- shows all the neighbours of current word, also creating unittests file nd writing some simple unittests

--- Database-Management_and_Partitioning_Files_txt.py
+++ Database-Management_and_Partitioning_Files_txt.py
@@ -1,13 +1,20 @@
 
+"""
+This file is used to create text files that are used as the main word databases
+for the game, categorizing words by length and also creating databases for partitionings.
+"""
 
-# 5- letter words are already ordered in csv file  and dont need
-# to be dealt with,they can be accessed immediately during a game
-# when file called to for example check words or something
 
 def sort_words_by_length_6(input_file, output_file):
     """
     This function reads words from the input file of 370k words and
     find the 6-letter words to add to a new file, where one word on every line
+
+    :param input_file: input file containing words
+    :type input_file: str
+    :param output_file: file where 6-letter words will be saved
+    :type output_file: str
+    :return: None
     """
     with open(input_file, 'r') as f:                            # open input file, read it, strip and split lines
         words = f.read().strip().splitlines()
@@ -28,6 +35,12 @@ def words_len5(input_file, output_file):
     """
     This function reads words from the input file of 370k words and
     find the 5-letter words to add to a new file, where one word on every line
+
+    :param input_file: input file containing words
+    :type input_file: str
+    :param output_file: file where 5-letter words will be saved
+    :type output_file: str
+    :return: None
     """
     with open(input_file, 'r') as f:
         words = f.read().strip().splitlines()
@@ -47,13 +60,19 @@ def words_len_4(input_file, output_file):
     """
     This function reads words from an input file containing ONLY 4-letter words
     and writes them to a new file where one word on every line, also making them lower case.
+
+    :param input_file: input file containing words
+    :type input_file: str
+    :param output_file: file where 4-letter words will be saved
+    :type output_file: str
+    :return: None
     """
     with open(input_file, 'r') as f:
-        words = f.read().strip().split()  # Split by spaces to get individual words
+        words = f.read().strip().split()                    # Split by spaces to get individual words
 
     # Write the 4-letter words to the output file
-    with open(output_file, 'w') as outfile:         # open new file
-        for word in words:                          # add the words one by one on new lines
+    with open(output_file, 'w') as outfile:                 # open new file
+        for word in words:                                  # add the words one by one on new lines
             outfile.write(word.lower().strip() + "\n")      # Write each word to the new file
 
 
@@ -61,9 +80,19 @@ words_len_4("databases/4letterwords.txt", "databases/4_letter_valid_words.txt")
 
 #----------------------------------------------------------------------------------------------------------------
 
-
 # Merges all 4-, 5- and 6-letter words into one file (namely all_words.txt)
 def merge_files(four, five, six):
+    """
+    This function merges 4-, 5-, and 6-letter words from different files into a single file.
+
+    :param four: file containing 4-letter words
+    :type four: str
+    :param five: file containing 5-letter words
+    :type five: str
+    :param six: file containing 6-letter words
+    :type six: str
+    :return: None
+    """
     with open(four, "r") as txt4, \
     open(five, "r") as txt5, \
     open(six, "r") as txt6, \
@@ -85,8 +114,10 @@ def load_partitions_from_file(filename):
     """
     This function loads partitions from partition.txt to a list of sets,
     that have a length of over 40 words in a partition.
-    :param filename: the partitions.txt file with the partitions
-    :return: return the partitions as lists to further process
+    :param filename: file containing partitions
+    :type filename: str
+    :return: list of sets, each set contains words in a partition
+    :rtype: list[set[str]]
     """
     with open(filename, "r") as file:
         partitions = eval(file.read())              # evaluate string to Pythong object (list of sets)
@@ -98,13 +129,17 @@ def load_partitions_from_file(filename):
 
     return partitions_sets                          # return all the partitioned sets
 
+# --------------------------------------------------------------------------------------------------------------
 
 def filter_partitions_by_word_length(partitions):
     """
     This function filters the partition sets of already over len 40 into a dictionary
     dependnig on the word_lengths it has
-    :param partitions: the partitions of len > 40
-    :return: dictionary with keys 4,5,6 for each word len with their partition
+
+    :param partitions: list of partition sets containing words
+    :type partitions: list[set[str]]
+    :return: Dictionary with keys 4, 5, and 6 for word lengths, each containing a list of partitions
+    :rtype: dict[int, list[set[str]]]
     """
     filtered_partitions = {4:[],                # making keys for each word length to store their partitions
                            5:[],
@@ -119,15 +154,17 @@ def filter_partitions_by_word_length(partitions):
 
     return filtered_partitions
 
+# --------------------------------------------------------------------------------------------------------------
 
 def write_partition_to_file(filtered_partitions):
     """
-    This function creates 3 new files with partitions for 4,5,6- letter words
-    :param filtered_partitions: the dictionary including the 3 partitions
-    :return: new txt files for each of the partitions
+    This function writes the partitions to separate files for 4, 5, and 6-letter words.
+
+    :param filtered_partitions: dictionary containing partitions for word lengths 4, 5, and 6
+    :type filtered_partitions: dict[int, list[set[str]]]
+    :return: None
     """
     for length in [4,5,6]:                                  # loop through neccesaary word lengths
-        #if filtered_partitions[length]:                     # access the dictionary keys by neccessary word_len value
         filename = f"databases/partitions_{length}.txt"     # create a filename based on length
 
         with open(filename, "w") as output_file:            # open new file
@@ -135,9 +172,10 @@ def write_partition_to_file(filtered_partitions):
                 for word in partition:                      # write every word in partition to a new line
                     output_file.write(word.upper().strip() + "\n")
 
-
+# --------------------------------------------------------------------------------------------------------------
+# Using the functions to create the partitions_.txt files for every word length
 partitions = load_partitions_from_file("databases/partitions.txt")    # get the partitions over length 40
 filtered_partition = filter_partitions_by_word_length(partitions)     # filter them into dictionary of 3,4,5- letter word partitions
 write_partition_to_file(filtered_partition)                           # create 3 new files based on word length including its partition over length 40
 
-
+# --------------------------------------------------------------------------------------------------------------
---
+++ unittests.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest.mock import patch
+from graph_traversal import all_possible_next_words
+from graph_traversal import choose_words
+from graph_traversal import find_neighbours
+
+"""
+This file includes the unittests for our functions and code.
+"""
+
+# 1- testing to see if the find find_all_next_word finds the correct neighbours, when called
+
+class TestGraphTraversal(unittest.TestCase):
+    def test_all_possible_next_words_mind(self):  # testing so see if it finds correct neighbours
+        word = "MIND"
+        expected_neighbours = ('BIND,FIND,HIND,KIND,LIND,RIND,SIND,TIND,WIND,MAND,MEND,MILD,MINA,MINE,MING,MINI,MINK,MINO,MINT,MINX,MINY',)
+        result = all_possible_next_words(word)
+        self.assertEqual(result,expected_neighbours)      # check to see if result matches expected
+
+    def invalid_word(self):  # testing for a word  not from database
+        word = "ZXYS"
+        expected_neighbours = ()
+        result = all_possible_next_words(word)
+        self.assertEqual(result, expected_neighbours)
+
+
+# if __name__ = "__main__":
+#     unittest.main()
+
+
