afd245109c16b7419bea3839ab4878b3b6a990d1
Mariela Pluutus <mariela.pluutus@gmail.com>
2024-11-13 16:55:44 +0100

    2 new functions for graph traversal

--- graph_traversal.py
+++ graph_traversal.py
@@ -1,12 +1,14 @@
+import random
 from collections import deque # BFS
 from generate_linked_database import create_adj_list
-import heapq # A*
-import sqlite3 # Needed to read in the database
+import heapq    # A*
+import sqlite3  # Needed to read in the database
 import time
 
 # Connects sqlite3 package to the database file, and creates a cursor object based on it
 database = sqlite3.connect("databases/linked_database.db")
 cursor_obj = database.cursor()
+# ----------------------------------------------------------------------------------------------------------------------
 def find_neighbours(word):
     """
     Finds all neighbours of a certain word.
@@ -31,6 +33,8 @@ def find_neighbours(word):
 
 # print(find_neighbours("BOAT"))
 
+# ----------------------------------------------------------------------------------------------------------------------
+
 def letter_difference(curr_word, neighbour):
     """
     Finds the amount of different letters based on two words.
@@ -48,6 +52,8 @@ def letter_difference(curr_word, neighbour):
         if x != y:
             difference += 1
     return difference
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 def a_pain_algorith(start_word, end_word):
     """
@@ -83,7 +89,7 @@ def a_pain_algorith(start_word, end_word):
 
         # Base case: if end has been reached, return entire path
         if curr_word == end_word:
-            return path, len(path)-1 # -1 to not include starting word in path length
+            return path, len(path)-1        # -1 to not include starting word in path length
 
         visited.add(curr_word)
         neighbours = find_neighbours(curr_word)
@@ -101,7 +107,9 @@ def a_pain_algorith(start_word, end_word):
                 heapq.heappush(priority_queue, (expected_cost, neighbour, new_cost, temp_path))
     return "Sorry, a path between the starting and end word wasn't found."
 
-# print(a_pain_algorith("FLOSS", "TEETH"))
+#print(a_pain_algorith("DEEK", "JOWS"))
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 def bfs_traversal(adj):
     """
@@ -113,26 +121,27 @@ def bfs_traversal(adj):
     """
     visited = set()
     partitions = []
-    for word in adj: # Go through words in adjacency list
+    for word in adj:                # Go through words in adjacency list
         if word not in visited:
-            partition = set() # creates a new partition for every new non visited element
+            partition = set()       # creates a new partition for every new non visited element
             q = deque([word])
 
-            while q: # fully explores a partition
+            while q:                            # fully explores a partition
                 curr_word = q.popleft()
-                if curr_word not in visited: # checks if the word is visited
-                    visited.add(curr_word) # adds to visited list
-                    partition.add(curr_word) # adds to current partition
+                if curr_word not in visited:    # checks if the word is visited
+                    visited.add(curr_word)      # adds to visited list
+                    partition.add(curr_word)    # adds to current partition
                     if curr_word in adj.keys():
                         for neighbour in adj[curr_word]:
                             q.append(neighbour) #appends all connections of the current word to the queue
-            partitions.append(partition) # when current partition is fully explored append it to list of partitions
+            partitions.append(partition)        # when current partition is fully explored append it to list of partitions
     with open("databases/partitions.txt", "w", newline= "\n") as file:
-        file.write(str(partitions)) # writes the partitions into a file
+        file.write(str(partitions))             # writes the partitions into a file
     return partitions
 
-words_filepath = "databases/all_words.txt" # gets the filepath for use in adjlist
-words = set() # creates a set to use in adjlist
+
+words_filepath = "databases/all_words.txt"      # gets the filepath for use in adjlist
+words = set()                                   # creates a set to use in adjlist
 
 # writes the words in file to words (set)
 with open(words_filepath, "r") as txt:
@@ -140,37 +149,117 @@ with open(words_filepath, "r") as txt:
         word = word.replace("\n", "").upper()
         words.add(word)
 
-def partition_filter(set):
-    """
-    Loops through all partitions and only returns sizeable ones
-    :param set: set of partitions which will be filtered
-    :type set: set
-    :return: filtered list of partitions
-    :rtype: list
-    """
-    partitions = [] # Creates a list for the partitions
-    for partition in set: # Loops through the partitions
-        if len(partition) > 40: # Checks if the partitions are large enough and appends them to the list if so
-            partitions.append(partition)
-    return partitions
+# ----------------------------------------------------------------------------------------------------------------------
 
-# The following function should be in databaseToTextfile.py
-def partition_to_file(partition, filename):
-    with open(filename, 'w') as outfile:
-        for word in partition:
-            outfile.write(word.lower().strip()+ "\n")
 
-adj = create_adj_list(words) # gets the adj list
-partitions = bfs_traversal(adj) #excecution of the partitioning
-print(len(partitions)) # Prints the length of the unfiltered partitions
-partitions = partition_filter(partitions) # Filters the partitions
-print(len(partitions)) # Prints the length of the filtered partitions
-partition = partitions[0]
-partition_to_file(partition, "databases/partition_6.txt")
-partition = partitions[1]
-partition_to_file(partition, "databases/partition_5.txt")
-partition = partitions[2]
-partition_to_file(partition, "databases/partition_4.txt")
+# function to select 2 words from a selected partition file and make sure they are at least
+def choose_words(word_len):
+    """
+    This function chooses 2 words from a partitioning depending on word length chosen
+    and makes sure the words are different enough, meaning at maximum 1 similar letter in the word
+    :param word_len: the chosen word length
+    :type word_len:
+    :return: list of 2 words that will be used as start and end of the game
+    :rtype: lst
+    """
+
+    filename = f"databases\partitions_{word_len}.txt"     # access the correct partitioning set from the right file
+    with open(filename, "r") as file:                     # open the file, get the set
+        words = eval(file.read().strip())
+
+    words_list = list(words)                              # convert set to list
+
+    while True:
+        start_word = random.choice(words_list)            # choose a random word for start
+        end_word = random.choice(words_list)              # random word as end
+
+        if start_word == end_word:                        # making sure the words are not the same
+            continue                                      # if same word, do while loop again, generating new start, end
+        # checking that there is only one letter in common
+        count = 0                                         # counter to make sure common letters not over 1
+        for let1 in start_word:                           # loop through the words of start
+            for let2 in end_word:                         # loop thrpugh words of end
+                if let1 == let2:
+                    count += 1                            # if they have same letters, up the count
+                    if count > 1:                         # if more than one common letter, regenerate 2 words
+                        break                             # exit let1 for loop, start again by generating new start and end
+        else:
+            start_and_end = []
+            start_and_end.append(start_word)
+            start_and_end.append(end_word)
+
+            return start_and_end                          # return start and end word as a list
+
+
+#print(choose_words(6))
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Connects sqlite3 package to the database file, and creates a cursor object based on it
+# database = sqlite3.connect("databases/linked_database.db")
+# cursor_obj = database.cursor()
+def all_possible_next_words(word):
+    """
+    This function, generates all the one letter difference words, to the word we need.
+    (e.g., all_possible_next_word(MIND) -> KIND, MIND, ... and so on)
+    :param word: the current word we need to find neighbours for
+    :type word: str
+    :return: all one letter difference words
+    :rtype: tuple
+    """
+    # access database, find the word, print all its neighbours
+    word = word.upper()
+    cursor_obj.execute("SELECT Neighbours FROM linked_database WHERE Word = ?", (word,))
+    neighbours = cursor_obj.fetchone()                  # Gets neighbours in the form of a tuple
+
+    return neighbours
+
+
+#print(all_possible_next_words("mind"))
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+#
+# def partition_filter(set):
+#     """
+#     Loops through all partitions and only returns sizeable ones
+#     :param set: set of partitions which will be filtered
+#     :type set: set
+#     :return: filtered list of partitions
+#     :rtype: list
+#     """
+#     partitions = [] # Creates a list for the partitions
+#     for partition in set: # Loops through the partitions
+#         if len(partition) > 40: # Checks if the partitions are large enough and appends them to the list if so
+#             partitions.append(partition)
+#     return partitions
+#
+# # The following function should be in databaseToTextfile.py
+# def partition_to_file(partition, filename):
+#     with open(filename, 'w') as outfile:
+#         for word in partition:
+#             outfile.write(word.lower().strip()+ "\n")
+#
+# adj = create_adj_list(words) # gets the adj list
+# partitions = bfs_traversal(adj) #excecution of the partitioning
+# print(len(partitions)) # Prints the length of the unfiltered partitions
+# partitions = partition_filter(partitions) # Filters the partitions
+# print(len(partitions)) # Prints the length of the filtered partitions
+# partition = partitions[0]
+# partition_to_file(partition, "databases/partition_6.txt")
+# partition = partitions[1]
+# partition_to_file(partition, "databases/partition_5.txt")
+# partition = partitions[2]
+# partition_to_file(partition, "databases/partition_4.txt")
+
+
+
+
+
+
+
 # def bfs_algorith(start_word, end_word):
 #     iteratable = (start_word, [start_word])
 #     queue = deque([iteratable]) # Initialises queue with starting word and path
