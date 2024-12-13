4cd8be6a7cf633ab0fef49c9163a26c1536ead31
Hazel <s3797457@vuw.leidenuniv.nl>
2024-12-13 15:49:16 +0100

    attempt to turn backend logic into frontend

--- Project_Code/main.py
+++ Project_Code/main.py
@@ -22,21 +22,56 @@ if generate_partitions:
 
 # Start game (class)
 if start_game:
-    mode = int(input("Which mode (4, 5, 6)? "))
+    # window when start game
+    ctk.set_appearance_mode("system")
+    ctk.set_default_color_theme("green")
+    root = ctk.CTk()
+    root.geometry("470x900")
+    root.protocol("WM_DELETE_WINDOW", lambda: quit_game(root))
+
+    # Adding a scrollbar to the window
+    scrollbar = Scrollbar(root)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+    # mode = int(input("Which mode (4, 5, 6)? "))
+    mode = 4   #I WILL CHANGE TO MULTIPLE OPTIONS
     game = Game(mode, graph.adj_list)
 
-    if shortest_path_print:
-        print(graph.a_pain_algorith(game.curr_word, game.end_word))
+    # if shortest_path_print:
+    #     print(graph.a_pain_algorith(game.curr_word, game.end_word))
 
     steps = 0
     while game.curr_word != game.end_word:
         steps += 1
         print("\ncurr", game.curr_word)
-        print("neighbours", game.curr_neighbours)
-        print("end", game.end_word)
-        user_input = input("Pick word from neighbours: ").strip().upper()
+        label = ctk.CTkLabel(root, text=str(game.curr_word), font=("Roboto", 20))
+        label.pack(pady=20)
 
-        move = game.make_move(user_input)
+        # print("neighbours", game.curr_neighbours)
+
+        for neighbour in game.curr_neighbours:
+            print(neighbour)
+            button = ctk.CTkButton(master=root,
+                               text=f"{neighbour}",
+                               command=button_clicked,
+                               font=("Arial", 12),
+                            )
+            button.pack(pady=10, padx=10)
+
+        # replace end word with button
+        # print("end", game.end_word)
+
+        # user_input = input("Pick word from neighbours: ").strip().upper()
+
+        label = ctk.CTkLabel(root, text=str(game.end_word), font=("Roboto", 20))
+        label.pack(pady=20)
+
+        #this part lowkey doesnt work
+        if button_clicked():
+            user_choice = button.cget("text")
+            print("im here")
+
+        move = game.make_move(user_choice)
         if move is False:
             continue
         if move is True:
